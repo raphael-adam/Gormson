1d1027b30ce29e5b091b00bc65d0314865badd9b
vagrant <vagrant@homestead>
2020-07-29 15:59:05 +0000

    added update method for database

--- app/Http/Controllers/LeaveController.php
+++ app/Http/Controllers/LeaveController.php
@@ -1,10 +1,43 @@
 <?php
 
+use App\Leave;
+
 namespace App\Http\Controllers;
 
+use App\Employee;
+use App\Leave;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class LeaveController extends Controller
 {
-    //
+    public function updateLeave()
+    {
+        $employee = Employee::updateOrCreate(
+            ['first_name' => 'Daniel'],
+            ['last_name' => 'Marx']
+        );
+
+        $employee->leave()->updateOrCreate(['vacation_id' => 256], ['vacation_begin' => 20200707]);
+
+    }
+
+    public function onLeave()
+    {
+        $today = Carbon::now();
+
+        return Leave::where('vacation_begin', '<=', $today)
+            ->where('vacation_end', '>=', $today)
+            ->get();
+    }
+
+    public function onLeaveNextWeek()
+    {
+        $today = Carbon::now();
+        $nextWeek = Carbon::now()->addWeek();
+
+        return Leave::where('vacation_begin', '>=', $today)
+            ->where('vacation_begin', '<=', $nextWeek)
+            ->get();
+    }
 }
