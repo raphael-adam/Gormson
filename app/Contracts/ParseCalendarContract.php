114d3693e23a8a24a01fe16047ca48af545323b8
vagrant <vagrant@homestead>
2020-07-29 13:07:10 +0000

    parsed calendar class

--- app/Contracts/ParseCalendarContract.php
+++ app/Contracts/ParseCalendarContract.php
@@ -3,8 +3,17 @@
 
 namespace App\Contracts;
 
-
-class ParseCalendarContract
+interface ParseCalendarContract
 {
+
+    public function parseCalendar();
+
+    public function getParsedCalendar();
+    public function setParsedCalendar();
+
+    public function getRawCalendar();
+    public function setRawCalendar();
+
+
 
 }
